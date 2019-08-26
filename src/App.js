59354821c82babe307b411f6dc578033479dbabf
Corey Hawkins <corey@Coreys-MacBook-Air.local>
2019-08-26 11:37:16 -0700

    Made boxes with click feature

--- src/App.js
+++ src/App.js
@@ -12,11 +12,13 @@ constructor(){
   render(){
     return (
       <div>
-      <h1>Simon</h1>
-      <Box id={1} color="#18d871" />
-      <Box id={2} color="#fc4d32" />
-      <Box id={3} color="#1d97d8" />
-      <Box id={4} color="#efd72c"/>
+        <h1>Simon</h1>
+        <div id="boxWrapper">
+        <Box id={1}  color="#18d871" />
+        <Box id={2}  color="#fc4d32" />
+        <Box id={3}  color="#1d97d8" />
+        <Box id={4}  color="#efd72c"/>
+        </div>
       </div>
     )
   }
@@ -25,8 +27,10 @@ constructor(){
 const Box = (props) => {
   console.log(props)
   return (
-    <div style={{backgroundColor: props.color}} >BOX {props.id}</div>
+    <div onClick={()=>console.log('trigger')} className ="boxStyling" style={{backgroundColor: props.color}} >BOX {props.id}</div>
   )
 }
+
+
 
 export default App;
